234ddb476c0ba059b8a007c7c564636ac7d14aa2
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-05-02 15:54:41 -0300

    correção de layout da página pesquisa

--- front/src/component/HomeContent/ContextoGrafico/index.jsx
+++ front/src/component/HomeContent/ContextoGrafico/index.jsx
@@ -3,13 +3,12 @@ import { Grafico } from "../grafico/grafico";
 import Navbar from "../../NavBar";
 import Footer from "../../Footer";
 
-
-export const contextoGrafico=()=>{
-<>
-  <Navbar />
-  <Grafico/>
-  <h1>OLá Prazrr</h1>
-  <Footer />
-</>
-
+export default function ContextoGrafico() {
+  return(
+    <>
+      <Navbar />
+      <Grafico/>
+      <Footer />
+    </>
+  );
 }
